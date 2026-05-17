0e4eef26a272202db808110d2ba927fcd92ec27f
truefusion3 <truefusion3@gmail.com>
2026-05-17 11:28:11 +1000

    Added .gitignore + progress on main

--- main.py
+++ main.py
@@ -26,22 +26,32 @@ library = "/Users/"+ user +"/Library" # user library folder
 logger.info("Library defined")
 
 setroot = "" # root folder for depth first search
+dirList = [] # List of directories to be set
+
+def exit():
+    quit()
 
 def initWindow():
     window.title("Omni File Explorer")
     window.minsize(500, 500)
     window.maxsize(1000, 1000)
-    window.geometry("750x750+375+100")
+    window.geometry("1000x700+375+100")
     tk.Label(window, text="Test").pack()
     window.lift()
     window.attributes('-topmost', True)
     window.focus_force()
+    tk.Button(window, text="search", command=search).pack()
     window.after(100, lambda: window.attributes('-topmost', False))
 
+def search():
+    for item in dirList:
+        print(len(dirList))
+        tk.Label(window, text=item).pack()
+        window.config(text="testing testing 1 2 3")
+
 def main():
-    # cwd = os.getcwd()
-    # cwd = os.chdir(downloads)
-    # print(os.listdir())
+    dirList = os.listdir()
+    print(dirList)
     initWindow()
     window.mainloop()
     
